8b6675769f16a76ebfa489561d67ecce438c4902
sudobot1 <93724507+sudobot1@users.noreply.github.com>
2021-11-04 20:30:33 +0300

    Add files via upload

--- MEILNU.lua
+++ MEILNU.lua
@@ -21,7 +21,7 @@ Port    = io.popen("echo ${SSH_CLIENT} | awk '{ port = $3 } END { print port }'"
 UpTime  = io.popen([[uptime | awk -F'( |,|:)+' '{if ($7=="min") m=$6; else {if ($7~/^day/) {d=$6;h=$8;m=$9} else {h=$6;m=$7}}} {print d+0,"days,",h+0,"hours,",m+0,"minutes"}']]):read('*a'):gsub('[\n\r]+', '')
 --     Source MEILNU     --
 local AutoSet = function() 
-if not DevAbs:get(Server.."من ") then 
+if not DevAbs:get(Server.."MEILNU") then 
 io.write('\27[1;35m\nالان ارسل ايدي المطور الاساسي -› ⤈\n\27[0;33;49m') 
 local DevId = io.read():gsub(' ','') 
 if tostring(DevId):match('%d+') then 
